65c80abf7a246bd4b72e53cd44f45f975c35c049
alatorrev <alatorrev@github.com>
2016-09-22 16:55:56 -0500

    Incorporacion de tecnologias para crear Reportes testExport.xhtml (pagina de prueba) ExportReportTest.java (Bean de prueba) web/Reportes/*.jrxml (Reporte en jasper de prueba)

---
+++ src/java/com/sev/bean/ExportReportTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sev.bean;
+
+import com.sev.conexion.Conexion;
+import java.io.IOException;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+/**
+ *
+ * @author a_latorre
+ */
+@ManagedBean
+@ViewScoped
+public class ExportReportTest {
+
+    public void export() throws JRException, IOException {
+        Conexion con = new Conexion();
+        Map<String, Object> parametros = new HashMap<String, Object>();
+        FacesContext context = FacesContext.getCurrentInstance();
+        ServletContext servleContext = (ServletContext) context.getExternalContext().getContext();
+        String dirReporte = "";
+        dirReporte = servleContext.getRealPath("/Reportes/testReport.jasper");
+        HttpServletResponse response = (HttpServletResponse) context.getExternalContext().getResponse();
+        response.addHeader("Content-disposition", "attachment;filename=ACM1PT.pdf");
+        response.setContentType("application/pdf");
+
+        JasperPrint impres = JasperFillManager.fillReport(dirReporte, parametros, con.getConnection());
+        JasperExportManager.exportReportToPdfStream(impres, response.getOutputStream());
+        context.responseComplete();
+    }
+}
